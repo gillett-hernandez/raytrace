e93b0e169357a373c02af9c929990d966583ed63
Gillett Hernandez <gillett.hernandez@gmail.com>
2019-09-19 12:14:42 -0700

    removed old files

--- rt4.py
+++ rt4.py
@@ -15,34 +15,43 @@ parser.add_argument("--height", type=int, default=1080)
 parser.add_argument("--bounces", type=int, default=3)
 parser.add_argument("--timeout", type=int, default=30)
 
+
 def extract(cond, x):
     if isinstance(x, numbers.Number):
         return x
     else:
         return np.extract(cond, x)
 
-class vec3():
+
+class vec3:
     def __init__(self, x, y, z):
         (self.x, self.y, self.z) = (x, y, z)
+
     def __mul__(self, other):
         return vec3(self.x * other, self.y * other, self.z * other)
+
     def __add__(self, other):
         return vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+
     def __sub__(self, other):
         return vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+
     def dot(self, other):
         return (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
+
     def __abs__(self):
         return self.dot(self)
+
     def norm(self):
         mag = np.sqrt(abs(self))
         return self * (1.0 / np.where(mag == 0, 1, mag))
+
     def components(self):
         return (self.x, self.y, self.z)
+
     def extract(self, cond):
-        return vec3(extract(cond, self.x),
-                    extract(cond, self.y),
-                    extract(cond, self.z))
+        return vec3(extract(cond, self.x), extract(cond, self.y), extract(cond, self.z))
+
     def place(self, cond):
         r = vec3(np.zeros(cond.shape), np.zeros(cond.shape), np.zeros(cond.shape))
         np.place(r.x, cond, self.x)
@@ -50,10 +59,12 @@ class vec3():
         np.place(r.z, cond, self.z)
         return r
 
-rgb = vec3
-FARAWAY = 1.0e39            # an implausibly huge distance
 
-def raytrace(L, O, D, scene, bounce = 0, max_bounces=2):
+rgb = vec3
+FARAWAY = 1.0e39  # an implausibly huge distance
+
+
+def raytrace(L, O, D, scene, bounce=0, max_bounces=2):
     # O is the ray origin, D is the normalized ray direction
     # scene is a list of Sphere objects (see below)
     # bounce is the number of the bounce, starting at zero for camera rays
@@ -71,8 +82,9 @@ def raytrace(L, O, D, scene, bounce = 0, max_bounces=2):
             color += cc.place(hit)
     return color
 
+
 class Sphere:
-    def __init__(self, center, r, diffuse, mirror = 0.5):
+    def __init__(self, center, r, diffuse, mirror=0.5):
         self.c = center
         self.r = r
         self.diffuse = diffuse
@@ -93,12 +105,12 @@ class Sphere:
         return self.diffuse
 
     def light(self, L, O, D, d, scene, bounce, max_bounces):
-        M = (O + D * d)                         # intersection point
-        N = (M - self.c) * (1. / self.r)        # normal
+        M = O + D * d  # intersection point
+        N = (M - self.c) * (1.0 / self.r)  # normal
         E = O
-        toL = (L - M).norm()                    # direction to light
-        toO = (E - M).norm()                    # direction to ray origin
-        nudged = M + N * .0001                  # M nudged to avoid itself
+        toL = (L - M).norm()  # direction to light
+        toO = (E - M).norm()  # direction to ray origin
+        nudged = M + N * 0.0001  # M nudged to avoid itself
 
         # Shadow: find if the point is shadowed or not.
         # This amounts to finding out if M can see the light
@@ -123,42 +135,55 @@ class Sphere:
         color += rgb(1, 1, 1) * np.power(np.clip(phong, 0, 1), 50) * seelight
         return color
 
+
 class CheckeredSphere(Sphere):
     def diffusecolor(self, M):
         checker = ((M.x * 2).astype(int) % 2) == ((M.z * 2).astype(int) % 2)
         return self.diffuse * checker
 
+
 def do_raytrace(L, E, Q, scene, max_bounces):
     return raytrace(L, E, (Q - E).norm(), scene, max_bounces=max_bounces)
 
-def main(args):
-    (w, h) = (args.width, args.height)         # Screen size
 
-    L = vec3(5, 5., -10)        # Point light position
-    E = vec3(0., 0.35, -1.)     # Eye position
+def main(args):
+    (w, h) = (args.width, args.height)  # Screen size
+
+    L = vec3(5, 5.0, -10)  # Point light position
+    E = vec3(0.0, 0.35, -1.0)  # Eye position
 
     scene = [
-        Sphere(vec3(.75, .1, 1.), .6, rgb(0, 0, 1)),
-        Sphere(vec3(-.75, .1, 2.25), .6, rgb(.5, .223, .5)),
-        Sphere(vec3(-2.75, .1, 3.5), .6, rgb(1., .572, .184)),
-        CheckeredSphere(vec3(0,-99999.5, 0), 99999, rgb(.75, .75, .75), 0.25),
-        ]
+        Sphere(vec3(0.75, 0.1, 1.0), 0.6, rgb(0, 0, 1)),
+        Sphere(vec3(-0.75, 0.1, 2.25), 0.6, rgb(0.5, 0.223, 0.5)),
+        Sphere(vec3(-2.75, 0.1, 3.5), 0.6, rgb(1.0, 0.572, 0.184)),
+        CheckeredSphere(vec3(0, -99999.5, 0), 99999, rgb(0.75, 0.75, 0.75), 0.25),
+    ]
 
     r = float(w) / h
     # Screen coordinates: x0, y0, x1, y1.
-    S = (-1., 1. / r + .25, 1., -1. / r + .25)
+    # fmt:off
+    S = (
+        -1.0,
+        1.0 / r + 0.25,
+        1.0,
+        -1.0 / r + 0.25
+    )
+    # fmt:on
     x = np.tile(np.linspace(S[0], S[2], w), h)
     y = np.repeat(np.linspace(S[1], S[3], h), w)
 
     t0 = time.time()
     # N = 4
     N = os.cpu_count()
+    Qs = [vec3(_x, _y, 0) for _x, _y in zip(np.split(x, N), np.split(y, N))]
     with multiprocessing.Pool(processes=N) as pool:
         print("starting pool execution")
-        Qs = [vec3(_x, _y, 0) for _x, _y in zip(np.split(x, N), np.split(y, N))]
+
         print(x.shape, y.shape)
         print("sending starmap order")
-        colors = pool.starmap(do_raytrace, [copy.deepcopy(tuple([L, E, sub, scene, args.bounces])) for sub in Qs])
+        colors = pool.starmap(
+            do_raytrace, [copy.deepcopy(tuple([L, E, sub, scene, args.bounces])) for sub in Qs]
+        )
         # colors = [pool.apply_async(do_raytrace, copy.deepcopy(tuple([L, E, sub, scene, args.bounces]))) for sub in Qs]
         print("getting results")
         # colors = [res.get(timeout=args.timeout) for res in colors]
@@ -167,14 +192,25 @@ def main(args):
         # import pdb
         # pdb.set_trace()
         print("merging results")
-        common_shape = next(c.shape for v in colors for c in v.components() if not isinstance(c, int))
-        color = rgb(*[np.concatenate([c if type(c) != int else np.zeros(common_shape) for c in comp]) for comp in zip(*[v.components() for v in colors])])
+        common_shape = next(
+            c.shape for v in colors for c in v.components() if not isinstance(c, int)
+        )
+        color = rgb(
+            *[
+                np.concatenate([c if type(c) != int else np.zeros(common_shape) for c in comp])
+                for comp in zip(*[v.components() for v in colors])
+            ]
+        )
     # Q = vec3(x, y, 0)
     # color = do_raytrace(L, E, Q, scene, args.bounces)
     print("Took", time.time() - t0)
 
-    new_rgb = [Image.fromarray((255 * np.clip(c, 0, 1).reshape((h, w))).astype(np.uint8), "L") for c in color.components()]
+    new_rgb = [
+        Image.fromarray((255 * np.clip(c, 0, 1).reshape((h, w))).astype(np.uint8), "L")
+        for c in color.components()
+    ]
     Image.merge("RGB", new_rgb).save("fig.png")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(parser.parse_args())
